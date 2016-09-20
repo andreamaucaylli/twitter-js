2d3643e7d32d0ceb95ebef19086327761dd9db79
Andrea Maucaylli <andreamaucaylli@gmail.com>
2016-09-19 20:58:40 -0500

    uploading v2

--- js/app.js
+++ js/app.js
@@ -1,11 +1,40 @@
 window.addEventListener("load", function() {
     var boton = document.getElementById("publicar");
-    boton.addEventListener("click", function(){
-    	var texto = document.getElementById("texto").value;
+    boton.disabled = true;
+    boton.addEventListener("click", function (e){
+    	e.preventDefault();
+    	var texto = document.getElementById("texto");
+    	mensajes(texto.value);
+ 		texto.value = "";
+	});
+
+	function mensajes (texto) {
     	var contenedorMensaje = document.createElement("div");
 		contenedorMensaje.innerHTML = texto;
-    	var mensaje = document.getElementById("mensaje");
-    	mensaje.insertBefore(contenedorMensaje, mensaje.childNodes[0]);
-		document.getElementById("texto").value = "";
-	});
+    	var mensajes = document.getElementById("mensaje");
+    	mensajes.insertBefore(contenedorMensaje, mensaje.childNodes[0]);
+	}
+
+    var texto = document.getElementById("texto");
+	texto.addEventListener("keypress", function (){
+		boton.disabled = false;
+		caracteres();
+	}) 
+
+	
+	function caracteres () {
+		var texto = document.getElementById("texto");
+		var cantidadCaracteres = texto.value.length;
+		var limite = 140;
+		var contador = document.getElementById("contador");
+		contador.innerHTML = limite - cantidadCaracteres;
+
+		if (cantidadCaracteres >= limite) {
+			contador.classList.add("red");
+			boton.disabled = true;
+		} else {
+			contador.classList.remove("red");
+		}
+	}
 });
+
